843a17aaf006de917a4fe6eac66992aefc74133f
chinedumc <chibokachinedum@gmail.com>
2025-11-11 23:57:46 +0100

    Create Ball and ball movement

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
-  time.sleep(0.1)
+  time.sleep(0.1) #To see the ball move in it's track
   screen.update()
   ball.move()
 
